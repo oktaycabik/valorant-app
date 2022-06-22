b40a64220407b5f71238fe5f2e04e545d80fd9ff
Oktay Çabik <oktaycabik2@gmail.com>
2022-06-22 13:59:13 +0300

    düzenleme

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require("dotenv-webpack");
 module.exports = {
   entry: ["@babel/polyfill", "./src/index.js"],
   output: {
-    path: path.resolve(__dirname, "bundles"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   plugins: [new Dotenv()],
